71d43421cc57ed5d9bf34f5fccc4d9c342d28426
Lundhede <m010795@hotmail.com>
2016-11-09 15:50:53 +0100

    Klasser oprettet og der er oprettet de basale attributer. ToString er blevet overridet i  nogle af klasserne.

---
+++ P3 Midwife/P3 Midwife/Midwife.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Midwife
+{
+    class Midwife : Employee
+    {
+        public Midwife(int id, string name, string password, int telephonenumber, string email) 
+            : base(id, name, password, telephonenumber, email)
+        {
+        }
+
+
+    }
+}
